eb24a31e9a16075c24560b9a96cd6c7f1aea2a59
DavisAndn <davis.andherson@gmail.com>
2023-10-15 11:59:13 +0530

    change popup icon to a square slider

--- popup.js
+++ popup.js
@@ -3,19 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Load the current state from storage
   chrome.storage.sync.get('extensionEnabled', function(data) {
-    button.textContent = data.extensionEnabled ? 'Turn Off' : 'Turn On';
+    button.checked = data.extensionEnabled;
   });
 
   // Toggle the extension state when the button is clicked
-  button.addEventListener('click', function() {
-    chrome.storage.sync.get('extensionEnabled', function(data) {
-      const newState = !data.extensionEnabled;
-      chrome.storage.sync.set({ 'extensionEnabled': newState }, function() {
-        button.textContent = newState ? 'Turn Off' : 'Turn On';
-        // Send a message to content scripts to update the state
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {extensionEnabled: newState});
-        });
+  button.addEventListener('change', function() {
+    const newState = button.checked;
+    chrome.storage.sync.set({ 'extensionEnabled': newState }, function() {
+      // Send a message to content scripts to update the state
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {extensionEnabled: newState});
       });
     });
   });
